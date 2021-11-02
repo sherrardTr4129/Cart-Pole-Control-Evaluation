feea8a90cd48b219b75e8cfe6e38f1462cff5887
Trevor Sherrard <sherrardtr@gmail.com>
2021-11-01 20:36:15 -0400

    add comments

--- DQN_method/cartpole.py
+++ DQN_method/cartpole.py
@@ -20,8 +20,9 @@ EXPLORATION_MAX = 1.0
 EXPLORATION_MIN = 0.01
 EXPLORATION_DECAY = 0.995
 
-class DQNSolver:
+MAX_EPISODES = 1000
 
+class DQNSolver:
     def __init__(self, observation_space, action_space):
         self.exploration_rate = EXPLORATION_MAX
 
@@ -58,32 +59,80 @@ class DQNSolver:
         self.exploration_rate = max(EXPLORATION_MIN, self.exploration_rate)
 
 
-def cartpole():
+def cartpole_training(render_training_image = False):
+    """
+    this function performs episodic training of the 
+    cart-pole DQN model using the Open AI Gym enviornment.
+    A copy of the model will be saved to "model/cartpole_model".
+
+    params:
+        render_training_image (bool): should the episode be visualized?
+    returns:
+        None
+    """
+    # set up enviornment for cart-pole
     env = gym.make(ENV_NAME)
+
+    # set up score logging object
     score_logger = ScoreLogger(ENV_NAME)
+
+    # set up observation and action space instnaces
     observation_space = env.observation_space.shape[0]
     action_space = env.action_space.n
+
+    # set up DQN objects
     dqn_solver = DQNSolver(observation_space, action_space)
     run = 0
+
+    # start episodic training
     while True:
+        # save model once we've reached MAX_EPISODES
+        if(run >= MAX_EPISODES):
+            dqn_solver.model.save("model/cartpole_model")
+            break
+
+        # reset enviornment before episode start
         run += 1
         state = env.reset()
         state = np.reshape(state, [1, observation_space])
         step = 0
+
+        # begin episode
         while True:
             step += 1
-            env.render()
+
+            # render training if flag was set
+            if(render_training_image):
+                env.render()
+
+            # get action from current state using DQN model
             action = dqn_solver.act(state)
+
+            # advance enviornment state by taking action
             state_next, reward, terminal, info = env.step(action)
+
+            # modify reward as positive or negative depending on outcome of movement
+            # (i.e. did we lose balance?)
             reward = reward if not terminal else -reward
+
+            # get next state
             state_next = np.reshape(state_next, [1, observation_space])
+
+            # append current state, action and reward to memory
             dqn_solver.remember(state, action, reward, state_next, terminal)
+
+            # increment state
             state = state_next
+
+            # if we have lose control, end the episode
+            # and log data
             if terminal:
                 print("Run: " + str(run) + ", exploration: " + str(dqn_solver.exploration_rate) + ", score: " + str(step))
                 score_logger.add_score(step, run)
                 break
+
+            # perform replay process
             dqn_solver.experience_replay()
 
 if __name__ == "__main__":
-    cartpole()
+    cartpole_training()
